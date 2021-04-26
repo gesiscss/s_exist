3ecbe4df7ff37b848aaf3d4ad81f06b39a67dec1
hide-ous <mattia.samory@gmail.com>
2021-04-26 16:07:18 +0200

    * helper to read train/test data

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
 import json
+import pandas as pd
+import os
 
 
 def read_config():
     with open('config.json') as f:
         config = json.load(f)
     return config
+
+
+def read_train():
+    config = read_config()
+    df = pd.read_csv(os.path.join(config['DATA_ROOT'], config['TRAINING_REL']), sep='\t',
+                     usecols=['id', 'source', 'language', 'text', 'task1', 'task2']).set_index('id')
+    return df
+
+
+def read_test():
+    config = read_config()
+    df = pd.read_csv(os.path.join(config['DATA_ROOT'], config['TEST_REL']), sep='\t',
+                     usecols=['id', 'source', 'language', 'text']).set_index('id')
+    return df
