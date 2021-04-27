a6d297a8bbee70d6f116d18c61efe11b03c65751
hide-ous <mattia.samory@gmail.com>
2021-04-27 16:32:12 +0200

    * added baselines to classification script

--- classify.py
+++ classify.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegressionCV, LogisticRegression, ElasticNet
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit, StratifiedKFold, cross_val_predict
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
@@ -14,7 +17,7 @@ if __name__ == '__main__':
 
     print('read training data')
     train = read_train(languages=['en'])
-    features = ['sif', 'senpai']
+    features = ['sif', 'senpai_unclustered', 'senpai']
     for feature in features:
         print('read precomputed feature', feature)
         feature_path = build_feature_path('TRAINING_REL', feature)
@@ -27,18 +30,34 @@ if __name__ == '__main__':
     print('encode platform')
     platform_le = LabelEncoder()
     train['source'] = platform_le.fit_transform(train.source)
-    features += ['source', 'language']
+    features += ['source', 'language', 'text']
 
-    X = train[[column for column in train.columns if any(column.startswith(feature) for feature in features)]].values
+    X = train[[column for column in train.columns if any(column.startswith(feature) for feature in features)]]
     y = train.task1.values
     y_le = LabelEncoder()
     y = y_le.fit_transform(y)
 
-    print(X.shape, y.shape, np.unique(y))
+    print('X.shape', X.shape, 'y.shape', y.shape, 'unique y', np.unique(y))
     labels = y_le.classes_
     ss = StandardScaler()
-    fs = SelectFromModel(estimator=ElasticNet(), max_features=200)
-    clf = Pipeline(steps=[('ss', ss), ('fs', fs), ('clf', RandomForestClassifier())])
+    fs = SelectFromModel(estimator=ElasticNet(), max_features=800)
+    clf = Pipeline(steps=[('cv', ColumnTransformer(transformers=[('cv', CountVectorizer(analyzer='char',
+                                                                                        ngram_range=(3, 4)), 'text')],
+                                                   remainder='passthrough')),
+                          # ('ss', ss),
+                          # ('fs', fs),
+                          ('clf', RandomForestClassifier())])
     skf = StratifiedKFold(n_splits=10)
     y_pred = cross_val_predict(estimator=clf, X=X, y=y, cv=skf)
+    print(classification_report(y_true=y, y_pred=y_pred, target_names=labels))
+
+    print('bow baseline')
+    y_pred = cross_val_predict(estimator=Pipeline(steps=[('vec', CountVectorizer()), ('clf', MultinomialNB())]),
+                               X=train.text, y=y, cv=skf)
+    print(classification_report(y_true=y, y_pred=y_pred, target_names=labels))
+
+    print('char ngram baseline')
+    y_pred = cross_val_predict(estimator=Pipeline(steps=[('vec', CountVectorizer(analyzer='char', ngram_range=(3, 4))),
+                                                         ('clf', MultinomialNB())]),
+                               X=train.text, y=y, cv=skf)
     print(classification_report(y_true=y, y_pred=y_pred, target_names=labels))
