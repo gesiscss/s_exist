36cba467682110af5ade259a5428266bae0e05bc
indiiigo <indira15021@iiitd.ac.in>
2021-04-30 22:50:53 +0200

    + training bert on train minus validation

--- mentions_and_hashtags.py
+++ mentions_and_hashtags.py
@@ -17,9 +17,16 @@ if __name__ == '__main__':
     train = read_train()
     test = read_test()
 
+    all_hashtags = train['text'].str.extractall(r'(\#\w+)')[0].value_counts().keys()
+    all_mentions = train['text'].str.extractall(r'(\@\w+)')[0].value_counts().keys()
+
+    hashtags_cv = CountVectorizer(vocabulary=list(set(all_hashtags)), token_pattern = r'\b\w\w+\b|(?<!\w)@\w+|(?<!\w)#\w+')
+    mentions_cv = CountVectorizer(vocabulary=list(set(all_mentions)), token_pattern = r'\b\w\w+\b|(?<!\w)@\w+|(?<!\w)#\w+')
+
     for dataset, dataset_key in [(train, 'TRAINING_REL'),
                                  (test, 'TEST_REL')]:
         
+    
         mention_list = list()
         hashtag_list = list()
 
@@ -36,4 +43,23 @@ if __name__ == '__main__':
 
         feature_store_path = build_feature_path(dataset_key, 'mention_count')
         df = dataset[['mention_count']]
+        df.to_csv(feature_store_path)
+
+        feature_store_path = build_feature_path(dataset_key, 'hashtags')
+        hashtags_matrix = hashtags_cv.fit_transform(dataset.text).todense()
+        df = pd.DataFrame(hashtags_matrix, index=dataset.index, columns=hashtags_cv.get_feature_names())
+        df.to_csv(feature_store_path)
+        
+        feature_store_path = build_feature_path(dataset_key, 'hashtags_total')
+        df = df.sum(axis=1)
+        df.to_csv(feature_store_path)
+
+
+        feature_store_path = build_feature_path(dataset_key, 'mentions')
+        mentions_matrix = mentions_cv.fit_transform(dataset.text).todense()
+        df = pd.DataFrame(mentions_matrix, index=dataset.index, columns=mentions_cv.get_feature_names())
+        df.to_csv(feature_store_path)
+        
+        feature_store_path = build_feature_path(dataset_key, 'mentions_total')
+        df = df.sum(axis=1)
         df.to_csv(feature_store_path)
--- train_bert.py
+++ train_bert.py
@@ -8,11 +8,11 @@ transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.WARNING)
 
 
-def train(data):
+def train(data, eval_df = None, savepath = "outputs/"):
 	# reference: https://github.com/ThilinaRajapakse/simpletransformers#a-quick-example
 
 	# Optional model configuration
-	model_args = ClassificationArgs(num_train_epochs=1)
+	model_args = ClassificationArgs(num_train_epochs=1, output_dir = savapath)
 
 	# Create a ClassificationModel
 	model = ClassificationModel(
@@ -21,6 +21,14 @@ def train(data):
 
 	# Train the model
 	model.train_model(data)
+
+	# Evaluate the model
+	if eval_df:
+		result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+		print(result)
+
+	# Make predictions with the model
+	# predictions, raw_outputs = model.predict(["Sam was a Wizard"])
 
 	return model
 
@@ -33,20 +41,24 @@ if __name__ == "__main__":
 	# - icwsm original data
 	# - icwsm original + counterfactual data
 
-	from utils import read_train, read_test, read_sexism
+	train_types = ["train_no_validation",
+				   "sexism",
+				   "train"
+				   ]
 
-	# data = read_train()
-	# print(data.head())
-	# data['labels'] = data['task1']
-	# data['labels'] = data['labels'].map({'sexist' : 1, 'non-sexist' : 0})
-	# model = train(data[['text', 'labels']])
+	from utils import read_train, read_test, read_sexism, read_train_no_validation, read_validation
 
-	data = read_sexism()
+	test = read_validation()
 	print(data.head())
 	data['labels'] = data['sexist']
 	data['labels'] = data['labels'].map({True : 1, False : 0})
-#	model = train(data[['text', 'labels']])
 
+	for train_type in train_types:
+		if train_type == 'train_no_validation':
+			data = read_train_no_validation()
+			print(data.head())
+			data['labels'] = data['sexist']
+			data['labels'] = data['labels'].map({True : 1, False : 0})
 
-
-
+	
+		model = train(data, test, savepath = "outputs/%s" train_type)
