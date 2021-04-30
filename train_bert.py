38adfa402bb2b2fbe87c01916ba364672fecc6d3
indiiigo <indira15021@iiitd.ac.in>
2021-04-30 23:05:22 +0200

    * fixed train_bert.py

--- train_bert.py
+++ train_bert.py
@@ -12,7 +12,7 @@ def train(data, eval_df = None, savepath = "outputs/"):
 	# reference: https://github.com/ThilinaRajapakse/simpletransformers#a-quick-example
 
 	# Optional model configuration
-	model_args = ClassificationArgs(num_train_epochs=1, output_dir = savapath)
+	model_args = ClassificationArgs(num_train_epochs=1, output_dir = savepath)
 
 	# Create a ClassificationModel
 	model = ClassificationModel(
@@ -41,24 +41,35 @@ if __name__ == "__main__":
 	# - icwsm original data
 	# - icwsm original + counterfactual data
 
-	train_types = ["train_no_validation",
+	train_types = ["train_no_validation", 
 				   "sexism",
-				   "train"
+				   "train",
 				   ]
+
+	label_field = {"train_no_validation" : "task1",
+				   "sexism" : "sexist",
+				   "train" : "task1"
+				   }
+
+	labels = {"train_no_validation" : {'sexist' : 1, 'non-sexist' : 0},
+				   "sexism" : {True : 1, False : 0},
+				   "train" : {'sexist' : 1, 'non-sexist' : 0},
+				   }
 
 	from utils import read_train, read_test, read_sexism, read_train_no_validation, read_validation
 
 	test = read_validation()
-	print(data.head())
-	data['labels'] = data['sexist']
-	data['labels'] = data['labels'].map({True : 1, False : 0})
+	print(test.head())
+	test['labels'] = test[label_field['train_no_validation']]
+	test['labels'] = test['labels'].map(labels['train_no_validation'])
+
 
 	for train_type in train_types:
 		if train_type == 'train_no_validation':
 			data = read_train_no_validation()
-			print(data.head())
-			data['labels'] = data['sexist']
-			data['labels'] = data['labels'].map({True : 1, False : 0})
-
+		
+		print(data.head())
+		data['labels'] = data[label_field[train_type]]
+		data['labels'] = data['labels'].map(labels[train_type])
 	
-		model = train(data, test, savepath = "outputs/%s" train_type)
+		model = train(data, test, savepath = "outputs/%s" %train_type)
