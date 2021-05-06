a1ddc39638b13a444b285ea03b7b15aac42d36f6
hide-ous <mattia.samory@gmail.com>
2021-05-06 11:30:17 +0200

    * reformat

--- classify.py
+++ classify.py
@@ -22,14 +22,13 @@ from sklearn.naive_bayes import BernoulliNB, ComplementNB, MultinomialNB
 from feature_selection import VIFSelector
 from preprocessing import preprocess
 from utils import read_train, read_test, build_feature_path, \
-read_modelpath, get_results, generate_test_run
+    read_modelpath, get_results, generate_test_run
 
 import pickle
 
 import time
 
 MODELPATH = read_modelpath()
-
 
 if __name__ == '__main__':
 
@@ -44,35 +43,35 @@ if __name__ == '__main__':
                 'senpai_selected',
                 'most_similar_scale_selected',
                 'perspective_selected',
-                'perspective_difference_selected', # file doesn't exist
+                'perspective_difference_selected',
                 'boosters_selected',
                 'hedges_selected',
                 # 'mentions_selected',
-                'hashtags_selected', # file doesn't exist
+                'hashtags_selected',
                 'mentions_total',
                 # 'hashtags_total',
                 # 'mentions_count',
                 # 'hashtags_count',
-                'bert_binary', # need to encode
-                'bert_multiclass', # need to encode
+                'bert_binary',  # need to encode
+                'bert_multiclass',  # need to encode
                 'bert_avg_all_but_first_binary_scaled',
-                'bert_avg_all_but_first_multiclass_scaled', 
+                'bert_avg_all_but_first_multiclass_scaled',
                 ]
     for data, data_type in [(train, "TRAINING_REL"), (test, "TEST_REL")]:
-        for feature in features:       
+        for feature in features:
             print('read precomputed feature', feature)
             feature_path = build_feature_path(data_type, feature)
             feature_df = pd.read_csv(feature_path, index_col='id')
             feature_df.columns = [feature + "_" + column for column in feature_df.columns]
             # encode the bert features, use the labels in the training data
             if feature == 'bert_binary' or feature == 'bert_multiclass':
-                label_mapping = {"bert_binary" : {'sexist' : 1, 'non-sexist' : 0},
-                           "bert_multiclass" : dict(zip(sorted(train.task2.unique()),
-                            range(0, len(train.task2.unique()))))
-                            }
+                label_mapping = {"bert_binary": {'sexist': 1, 'non-sexist': 0},
+                                 "bert_multiclass": dict(zip(sorted(train.task2.unique()),
+                                                             range(0, len(train.task2.unique()))))
+                                 }
 
-                feature_df['%s_predictions' %(feature)] = feature_df['%s_predictions' %(feature)]\
-                  .map(label_mapping[feature])
+                feature_df['%s_predictions' % feature] = feature_df['%s_predictions' % feature] \
+                    .map(label_mapping[feature])
             data = pd.merge(data, feature_df, how='left', left_index=True, right_index=True)
 
     print('encode language')
@@ -82,7 +81,7 @@ if __name__ == '__main__':
     print('preprocess text')
     train['text'] = train.text.apply(partial(preprocess, fix_encoding=True))
     test['text'] = test.text.apply(partial(preprocess, fix_encoding=True))
-    
+
     features += ['language', 'text']
 
     # try on a small sample to see if script works
@@ -95,7 +94,6 @@ if __name__ == '__main__':
 
     X_test = test[[column for column in test.columns if any(column.startswith(feature) for feature in features)]]
 
-
     # print(X['language'])
     # print(features)
 
@@ -106,9 +104,9 @@ if __name__ == '__main__':
 
     clf = Pipeline(steps=[
         ('cv', ColumnTransformer(transformers=[('cv', Pipeline(steps=[('cv', CountVectorizer(analyzer='char',
-                                                                      max_df=.5,
-                                                                      min_df=.1,
-                                                                      ngram_range=(3, 4))),
+                                                                                             max_df=.5,
+                                                                                             min_df=.1,
+                                                                                             ngram_range=(3, 4))),
                                                                       ('fs', fs)
                                                                       ]), 'text')],
                                  remainder='passthrough'
@@ -136,20 +134,18 @@ if __name__ == '__main__':
     print(classification_report(y_true=y, y_pred=y_pred, target_names=labels))
     clf.fit(X.text, y)
     test['predictions'] = clf.predict(X_test.text)
-    test['predictions'] = test['predictions'].map({1 : 'sexist', 0 : 'non-sexist'})
+    test['predictions'] = test['predictions'].map({1: 'sexist', 0: 'non-sexist'})
     test = test[['test_case', 'id', 'predictions']]
-    generate_test_run(test, task = "task1", run = "2", model_type = "bow")
+    generate_test_run(test, task="task1", run="2", model_type="bow")
 
     print('char ngram baseline')
     clf = Pipeline(steps=[('vec', CountVectorizer(analyzer='char', ngram_range=(3, 4))),
-                                                         ('clf', MultinomialNB())])
+                          ('clf', MultinomialNB())])
     y_pred = cross_val_predict(estimator=clf,
                                X=train.text, y=y, cv=skf)
     print(classification_report(y_true=y, y_pred=y_pred, target_names=labels))
     clf.fit(X.text, y)
     test['predictions'] = clf.predict(X_test.text)
-    test['predictions'] = test['predictions'].map({1 : 'sexist', 0 : 'non-sexist'})
+    test['predictions'] = test['predictions'].map({1: 'sexist', 0: 'non-sexist'})
     test = test[['test_case', 'id', 'predictions']]
-    generate_test_run(test, task = "task1", run = "2", model_type = "char_ngram")
-
-    
+    generate_test_run(test, task="task1", run="2", model_type="char_ngram")
